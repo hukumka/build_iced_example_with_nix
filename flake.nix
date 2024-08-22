cf526c88525b83e98d7c135a973d23fdb1342b91
Hukumka <hukumka212@gmail.com>
2024-08-22 12:40:04 +0700

    cargo2nix example

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Iced example";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ...}: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {self', pkgs, system, ...}:
+        let
+          rustVersion = "1.76.0";
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [inputs.cargo2nix.overlays.default (import inputs.rust-overlay)];
+          };
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            inherit rustVersion;
+            packageFun = import ./Cargo.nix;
+            packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "rust-analyzer";
+                overrideAttrs = drv: {
+                  propagatedNativeBuildInputs = drv.propagatedNativeBuildInputs or [ ] ++ (with pkgs; [
+                    pkg-config
+                    wayland
+                    libxkbcommon
+                    libGL
+                  ]);
+                };
+              })
+            ];
+          };
+        in {
+          packages = rec {
+            iced_nix = (rustPkgs.workspace.counter {}).bin;
+            default = iced_nix;
+          };
+          devShells.default = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              pkg-config
+              wayland
+              libxkbcommon
+              libGL
+              
+              rust-analyzer-unwrapped
+              (rust-bin.stable.${rustVersion}.default.override { extensions = [ "rust-src" ]; })
+            ];
+            LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath buildInputs}";
+          };
+        };
+    };
+}
